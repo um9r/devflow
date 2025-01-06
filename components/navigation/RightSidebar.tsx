036b757d67c93b068877231580e3d1e8f8ddf2c9
Umar <faroukumar99@gmail.com>
2025-01-06 14:53:48 +0100

    Implement Left & Right Sidebar

---
+++ components/navigation/RightSidebar.tsx
@@ -0,0 +1,76 @@
+import ROUTES from "@/constants/routes";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import TagCard from "../cards/TagCard";
+
+const hotQuestions = [
+  { _id: "1", title: "How to create a custom hook in React?" },
+  { _id: "2", title: "How to use React Router?" },
+  { _id: "3", title: "What is the difference between React and React Native?" },
+  { _id: "4", title: "How to use React Context?" },
+  { _id: "5", title: "How to use React Hooks?" },
+];
+
+const popularTags = [
+  { _id: "1", name: "react", questions: 100 },
+  { _id: "2", name: "javascript", questions: 200 },
+  { _id: "3", name: "typescript", questions: 130 },
+  { _id: "4", name: "nextjs", questions: 80 },
+  { _id: "5", name: "nodejs", questions: 40 },
+];
+
+const RightSidebar = () => {
+  return (
+    <section
+      className="pt-36 custom-scrollbar background-light900_dark200
+   light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6
+     overflow-y-auto border-l p-6 shadow-light-300 
+    dark:shadow-none max-xl:hidden"
+    >
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+
+        <div className="mt-7 flex flex-col w-full gap-[30px]">
+          {hotQuestions.map(({ _id, title }) => (
+            <Link
+              key={_id}
+              href={ROUTES.PROFILE(_id)}
+              className="flex cursor-pointer
+            items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">{title}</p>
+
+              <Image
+                src="/icons/chevron-right.svg"
+                alt="Chevron"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map(({ _id, name, questions }) => (
+            <TagCard
+              key={_id}
+              _id={_id}
+              name={name}
+              questions={questions}
+              showCount
+              compact
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
