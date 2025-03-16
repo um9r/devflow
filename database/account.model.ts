ac918f333a7debd6654c5648aaddc5e7e2ea4cb7
Umar <faroukumar99@gmail.com>
2025-03-16 18:17:36 +0100

    Refactor Mongoose models to include Document interface and improve type definitions

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,14 +1,15 @@
-import { model, models, Schema, Types } from "mongoose";
+import { Schema, model, models, Types, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
   image?: string;
   password?: string;
-  provider: string;
+  provider: string; // e.g., 'github', 'google', 'credentials'
   providerAccountId: string;
 }
 
+export interface IAccountDoc extends IAccount, Document {}
 const AccountSchema = new Schema<IAccount>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
@@ -21,6 +22,6 @@ const AccountSchema = new Schema<IAccount>(
   { timestamps: true }
 );
 
-const Account = models.Account || model<IAccount>("Account", AccountSchema);
+const Account = models?.Account || model<IAccount>("Account", AccountSchema);
 
 export default Account;
