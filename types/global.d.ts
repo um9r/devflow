acff393273de4b9aa35ac9463f6aaaf5f0df4757
Umar <faroukumar99@gmail.com>
2025-03-17 11:11:44 +0100

    Implement Create & Read User Routes

--- types/global.d.ts
+++ types/global.d.ts
@@ -19,3 +19,19 @@ interface Question {
   answers: number;
   views: number;
 }
+
+type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+type APIErrorResponse = NextResponse<ErrorResponse>;
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
