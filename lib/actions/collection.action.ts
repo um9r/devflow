b31a9a017dca702da2777fc6b3041761ccfe9275
Umar <faroukumar99@gmail.com>
2025-03-22 11:17:15 +0100

    Implement Collections Page

--- lib/actions/collection.action.ts
+++ lib/actions/collection.action.ts
@@ -108,6 +108,7 @@ export async function getSavedQuestions(
   const validationResult = await action({
     params,
     schema: PaginatedSearchParamsSchema,
+    authorize: true,
   });
 
   if (validationResult instanceof Error) {
