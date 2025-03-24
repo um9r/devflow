063dd608643e18f8afdb943edc6d704272e47819
Umar <faroukumar99@gmail.com>
2025-03-24 10:17:41 +0100

    Feat: Show Top Questions and Tags

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,8 +15,7 @@ import {
   IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
-import { revalidatePath } from "next/cache";
-import ROUTES from "@/constants/routes";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -318,11 +317,26 @@ export async function incrementViews(
 
     await question.save();
 
-    revalidatePath(ROUTES.QUESTION(questionId));
-
     return {
       success: true,
       data: { views: question.views },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
+  try {
+    await dbConnect();
+
+    const questions = await Question.find()
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
