aa9999abf1cdcffdf7062d15755afee7caf85b3a
Umar <faroukumar99@gmail.com>
2025-03-25 12:33:38 +0100

    Feat: Implement User Tags

--- lib/validations.ts
+++ lib/validations.ts
@@ -207,3 +207,7 @@ export const GetUserQuestionsSchema = PaginatedSearchParamsSchema.extend({
 export const GetUsersAnswersSchema = PaginatedSearchParamsSchema.extend({
   userId: z.string().min(1, { message: "User ID is required." }),
 });
+
+export const GetUsersTagsSchema = z.object({
+  userId: z.string().min(1, { message: "User ID is required." }),
+});
