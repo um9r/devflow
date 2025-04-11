a00b7ca1ca03640bc6ae19bc6cc376e7f75b972e
Umar <faroukumar99@gmail.com>
2025-04-11 10:55:03 +0100

    Fix: Update API_BASE_URL

--- lib/api.ts
+++ lib/api.ts
@@ -5,7 +5,8 @@ import { IUser } from "@/database/user.model";
 import { fetchHandler } from "./handlers/fetch";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://devflow-nu-five.vercel.app/api";
 
 export const api = {
   auth: {
